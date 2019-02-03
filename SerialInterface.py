523cd7a5f91bda6ec2eebb3509c4ddc5bfe9662d
Bryan Hoang <bryan.c.hoang@gmail.com>
2019-02-03 03:15:03 -0500

    Rename methods for style and add empty docstrings for documentation later.

--- SerialInterface.py
+++ SerialInterface.py
@@ -1,23 +1,51 @@
+"""
+
+"""
+
 import serial
 
-ser = serial.Serial('/dev/tty96B0', 9600)
+SER = serial.Serial('/dev/tty96B0', 9600)
 
 
 def clockwise(deg):
+    """
+
+
+    :param deg:
+    :return:
+    """
     cmd = 'a' + '{:03d}'.format(deg) + '.'
-    sendCMD(cmd)
+    send_cmd(cmd)
 
 
-def counterClockwise(deg):
+def counter_clockwise(deg):
+    """
+
+
+    :param deg:
+    :return:
+    """
     cmd = 'b' + '{:03d}'.format(deg) + '.'
-    sendCMD(cmd)
+    send_cmd(cmd)
 
 
 def launch(n):
+    """
+
+
+    :param n:
+    :return:
+    """
     cmd = 'c' + '{:03d}'.format(n) + '.'
-    sendCMD(cmd)
+    send_cmd(cmd)
 
 
-def sendCMD(data):
+def send_cmd(data):
+    """
+
+
+    :param data:
+    :return:
+    """
     for i in data:
-        ser.write(ord(i).to_bytes(1, byteorder='big'))
+        SER.write(ord(i).to_bytes(1, byteorder='big'))
