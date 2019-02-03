586520cedcef0608eff5feeacf43dac4d715b6e8
Jared McGrath <jaredws35@gmail.com>
2019-02-02 20:31:41 -0500

    Serial communication updated, motor control

---
+++ SerialInterface.py
@@ -0,0 +1,23 @@
+import serial
+
+ser = serial.Serial('/dev/tty96B0', 9600)
+
+
+def clockwise(deg):
+    cmd = 'a' + '{:03d}'.format(deg) + '.'
+    sendCMD(cmd)
+
+
+def counterClockwise(deg):
+    cmd = 'b' + '{:03d}'.format(deg) + '.'
+    sendCMD(cmd)
+
+
+def launch(n):
+    cmd = 'c' + '{:03d}'.format(n) + '.'
+    sendCMD(cmd)
+
+
+def sendCMD(data):
+    for i in data:
+        ser.write(ord(i))
--- ctrl/ctrl.ino
+++ ctrl/ctrl.ino
@@ -1,183 +1,138 @@
-int pwm1=9;
-int pwm2=10;
+// Define pins
 int ctr_a =9;
 int ctr_b =8;
 int ctr_c =11;
 int ctr_d =10;
-int sd =6;
+int l = 12;
+// Counter and delay vars
 int i=0;
-int t=1500;
+int d=1000;
+// Serial stream
+char data[64];
+char term = '.';
+int len = 0;
+int val = 0;
 
 void setup()
 {
-    //pinMode(sd,OUTPUT);
-    //pinMode(pwm1,OUTPUT);
-    //pinMode(pwm2,OUTPUT);
     pinMode(ctr_a,OUTPUT);
     pinMode(ctr_b,OUTPUT);
     pinMode(ctr_c,OUTPUT);
-    pinMode(ctr_d,OUTPUT); 
-    delay(1);
-    //digitalWrite(sd,HIGH);
-    //digitalWrite(pwm1,HIGH);
-    //digitalWrite(pwm2,HIGH);
-//    digitalWrite(ctr_a,LOW);
-//    digitalWrite(ctr_b,LOW);
-//    digitalWrite(ctr_c,LOW);
-//    digitalWrite(ctr_d,LOW);
-
+    pinMode(ctr_d,OUTPUT);
+    pinMode(l, OUTPUT);
+    Serial.begin(9600);
 }
 
 
 void loop ()
 {
-//   for(i=1500;i>=1;i--)
-//   {
-//       digitalWrite(ctr_a,HIGH);//A
-//       digitalWrite(ctr_b,LOW);
-//       digitalWrite(ctr_c,LOW);
-//       digitalWrite(ctr_d,LOW);
-//       delay(t);
-//       digitalWrite(ctr_a,HIGH);
-//       digitalWrite(ctr_b,HIGH);//AB
-//       digitalWrite(ctr_c,LOW);
-//       digitalWrite(ctr_d,LOW);
-//       delay(t);
-//       digitalWrite(ctr_a,LOW);
-//       digitalWrite(ctr_b,HIGH);//B
-//       digitalWrite(ctr_c,LOW);
-//       digitalWrite(ctr_d,LOW);
-//       delay(t);
-//       digitalWrite(ctr_a,LOW);
-//       digitalWrite(ctr_b,HIGH);
-//       digitalWrite(ctr_c,HIGH);//BC
-//        digitalWrite(ctr_d,LOW);
-//       delay(t);
-//       digitalWrite(ctr_a,LOW);
-//       digitalWrite(ctr_b,LOW);
-//       digitalWrite(ctr_c,HIGH);//C
-//       digitalWrite(ctr_d,LOW);
-//       delay(t);
-//       digitalWrite(ctr_a,LOW);
-//       digitalWrite(ctr_b,LOW);
-//       digitalWrite(ctr_c,HIGH);//CD
-//       digitalWrite(ctr_d,HIGH);
-//       delay(t);
-//        digitalWrite(ctr_a,LOW);
-//       digitalWrite(ctr_b,LOW);
-//       digitalWrite(ctr_c,LOW);//D
-//       digitalWrite(ctr_d,HIGH);
-//       delay(t);
-//        digitalWrite(ctr_a,HIGH);
-//       digitalWrite(ctr_b,LOW);
-//       digitalWrite(ctr_c,LOW);//DA
-//       digitalWrite(ctr_d,HIGH);
-//       delay(t);
-//       
-//   }
-//       digitalWrite(ctr_a,LOW);
-//       digitalWrite(ctr_b,LOW);
-//       digitalWrite(ctr_c,LOW);
-//       digitalWrite(ctr_d,LOW);
-//   
-//   
-//   
-//     for(i=1500;i>=1;i--)
-//   {
-//     
-//       digitalWrite(ctr_d,HIGH);//D
-//       delay(t);
-//       digitalWrite(ctr_c,HIGH);//DC
-//       delay(t);
-//       digitalWrite(ctr_d,LOW);//C
-//       delay(t);
-//       digitalWrite(ctr_b,HIGH);//CB
-//       delay(t);
-//       digitalWrite(ctr_c,LOW);//B
-//       delay(t);
-//       digitalWrite(ctr_a,HIGH);//BA
-//       delay(t);
-//       digitalWrite(ctr_b,LOW);//A
-//       delay(t);
-//       digitalWrite(ctr_d,HIGH);//AD
-//       delay(t);
-//       digitalWrite(ctr_a,LOW);
-//       digitalWrite(ctr_d,LOW);
-//   }
+  if (Serial.available() > 0) {
+    len = Serial.readBytesUntil(term, data, 64);
+    val = int(data[1])*100 + int(data[2])*10 + int(data[3]);
+    if (data[0] == 'a') {
+      clockwise(val);
+    }
+    else if (data[0] == 'b') {
+      cclockwise(val);
+    }
+    else {
+      launch(val);
+    }
+  }
+}
 
-   for(i=1500;i>=1;i--)
-   {
-       digitalWrite(ctr_a,LOW);//A
-       digitalWrite(ctr_b,HIGH);
-       digitalWrite(ctr_c,HIGH);
-       digitalWrite(ctr_d,HIGH);
-       delayMicroseconds(t);
-       digitalWrite(ctr_a,LOW);
-       digitalWrite(ctr_b,LOW);//AB
-       digitalWrite(ctr_c,HIGH);
-       digitalWrite(ctr_d,HIGH);
-       delayMicroseconds(t);
-       digitalWrite(ctr_a,HIGH);
-       digitalWrite(ctr_b,LOW);//B
-       digitalWrite(ctr_c,HIGH);
-       digitalWrite(ctr_d,HIGH);
-       delayMicroseconds(t);
-       digitalWrite(ctr_a,HIGH);
-       digitalWrite(ctr_b,LOW);
-       digitalWrite(ctr_c,LOW);//BC
-        digitalWrite(ctr_d,HIGH);
-       delayMicroseconds(t);
-       digitalWrite(ctr_a,HIGH);
-       digitalWrite(ctr_b,HIGH);
-       digitalWrite(ctr_c,LOW);//C
-       digitalWrite(ctr_d,HIGH);
-       delayMicroseconds(t);
-       digitalWrite(ctr_a,HIGH);
-       digitalWrite(ctr_b,HIGH);
-       digitalWrite(ctr_c,LOW);//CD
-       digitalWrite(ctr_d,LOW);
-       delayMicroseconds(t);
-        digitalWrite(ctr_a,HIGH);
-       digitalWrite(ctr_b,HIGH);
-       digitalWrite(ctr_c,HIGH);//D
-       digitalWrite(ctr_d,LOW);
-       delayMicroseconds(t);
-       digitalWrite(ctr_a,LOW);
-       digitalWrite(ctr_b,HIGH);
-       digitalWrite(ctr_c,HIGH);//DA
-       digitalWrite(ctr_d,LOW);
-       delayMicroseconds(t);
+void clockwise(int t) {
+  for (i = 0; i < t; i++) {
+     digitalWrite(ctr_a,LOW);//A
+     digitalWrite(ctr_b,HIGH);
+     digitalWrite(ctr_c,HIGH);
+     digitalWrite(ctr_d,HIGH);
+     delayMicroseconds(d);
+     digitalWrite(ctr_a,LOW);
+     digitalWrite(ctr_b,LOW);//AB
+     digitalWrite(ctr_c,HIGH);
+     digitalWrite(ctr_d,HIGH);
+     delayMicroseconds(d);
+     digitalWrite(ctr_a,HIGH);
+     digitalWrite(ctr_b,LOW);//B
+     digitalWrite(ctr_c,HIGH);
+     digitalWrite(ctr_d,HIGH);
+     delayMicroseconds(d);
+     digitalWrite(ctr_a,HIGH);
+     digitalWrite(ctr_b,LOW);
+     digitalWrite(ctr_c,LOW);//BC
+     digitalWrite(ctr_d,HIGH);
+     delayMicroseconds(d);
+     digitalWrite(ctr_a,HIGH);
+     digitalWrite(ctr_b,HIGH);
+     digitalWrite(ctr_c,LOW);//C
+     digitalWrite(ctr_d,HIGH);
+     delayMicroseconds(d);
+     digitalWrite(ctr_a,HIGH);
+     digitalWrite(ctr_b,HIGH);
+     digitalWrite(ctr_c,LOW);//CD
+     digitalWrite(ctr_d,LOW);
+     delayMicroseconds(d);
+     digitalWrite(ctr_a,HIGH);
+     digitalWrite(ctr_b,HIGH);
+     digitalWrite(ctr_c,HIGH);//D
+     digitalWrite(ctr_d,LOW);
+     delayMicroseconds(d);
+     digitalWrite(ctr_a,LOW);
+     digitalWrite(ctr_b,HIGH);
+     digitalWrite(ctr_c,HIGH);//DA
+     digitalWrite(ctr_d,LOW);
+     delayMicroseconds(d);
+  }
+}
 
-   }
-//       digitalWrite(ctr_a,HIGH);
-//       digitalWrite(ctr_b,HIGH);
-//       digitalWrite(ctr_c,HIGH);
-//       digitalWrite(ctr_d,HIGH);
-//   
+void cclockwise(int t) {
+  for (i = 0; i < t; i++) {
+     digitalWrite(ctr_a,LOW);//A
+     digitalWrite(ctr_b,HIGH);
+     digitalWrite(ctr_c,HIGH);
+     digitalWrite(ctr_d,HIGH);
+     delayMicroseconds(d);
+     digitalWrite(ctr_a,LOW);
+     digitalWrite(ctr_b,HIGH);//AD
+     digitalWrite(ctr_c,HIGH);
+     digitalWrite(ctr_d,LOW);
+     delayMicroseconds(d);
+     digitalWrite(ctr_a,HIGH);
+     digitalWrite(ctr_b,HIGH);//D
+     digitalWrite(ctr_c,HIGH);
+     digitalWrite(ctr_d,LOW);
+     delayMicroseconds(d);
+     digitalWrite(ctr_a,HIGH);
+     digitalWrite(ctr_b,HIGH);
+     digitalWrite(ctr_c,LOW);//DC
+     digitalWrite(ctr_d,LOW);
+     delayMicroseconds(d);
+     digitalWrite(ctr_a,HIGH);
+     digitalWrite(ctr_b,HIGH);
+     digitalWrite(ctr_c,LOW);//C
+     digitalWrite(ctr_d,HIGH);
+     delayMicroseconds(d);
+     digitalWrite(ctr_a,HIGH);
+     digitalWrite(ctr_b,LOW);
+     digitalWrite(ctr_c,LOW);//CB
+     digitalWrite(ctr_d,HIGH);
+     delayMicroseconds(d);
+     digitalWrite(ctr_a,HIGH);
+     digitalWrite(ctr_b,LOW);
+     digitalWrite(ctr_c,HIGH);//B
+     digitalWrite(ctr_d,HIGH);
+     delayMicroseconds(d);
+     digitalWrite(ctr_a,LOW);
+     digitalWrite(ctr_b,LOW);
+     digitalWrite(ctr_c,HIGH);//BA
+     digitalWrite(ctr_d,HIGH);
+     delayMicroseconds(d);
+  }
+}
 
-
-//     for(i=1500;i>=1;i--)
-//   {
-//     
-//       digitalWrite(ctr_d,HIGH);//D
-//       delay(t);
-//       digitalWrite(ctr_c,HIGH);//DC
-//       delay(t);
-//       digitalWrite(ctr_d,LOW);//C
-//       delay(t);
-//       digitalWrite(ctr_b,HIGH);//CB
-//       delay(t);
-//       digitalWrite(ctr_c,LOW);//B
-//       delay(t);
-//       digitalWrite(ctr_a,HIGH);//BA
-//       delay(t);
-//       digitalWrite(ctr_b,LOW);//A
-//       delay(t);
-//       digitalWrite(ctr_d,HIGH);//AD
-//       delay(t);
-//       digitalWrite(ctr_a,LOW);
-//       digitalWrite(ctr_d,LOW);
-//   }
-
-
+void launch(int t) {
+  digitalWrite(l, HIGH);
+  delay(t);
+  digitalWrite(l, LOW);
 }
